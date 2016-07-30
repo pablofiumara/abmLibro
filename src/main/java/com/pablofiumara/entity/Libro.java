55c0937c398b822d9a513d0d0ef180051711d783
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-07-30 20:26:33 -0300

    agregue entidad

---
+++ src/main/java/com/pablofiumara/entity/Libro.java
@@ -0,0 +1,43 @@
+package com.pablofiumara.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Libros")
+public class Libro {
+
+   private Integer Id;
+   private String titulo;
+
+
+   public Libro() {
+   }
+
+   public Libro(Integer unId, String unTitulo) {
+       this.Id = unId;
+       this.titulo = unTitulo;
+   }
+
+   @Id
+   @Column(name = "numeroDeId", nullable = false)
+   public Integer getId() {
+       return Id;
+   }
+
+   public void setId(Integer id) {
+       this.Id = id;
+   }
+
+   @Column(name = "tituloDelLibro", length = 20)
+   public String getTitulo() {
+       return titulo;
+   }
+
+   public void setTitulo(String unTitulo) {
+       this.titulo = unTitulo;
+   }
+}
