fa2834a59aa63901ba099ee212f5bcbdf0be780a
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-03 15:24:04 -0300

    agregue una interfaz servicio y su implementacion

---
+++ src/main/java/com/pablofiumara/service/LibroServicio.java
@@ -0,0 +1,15 @@
+package com.pablofiumara.service;
+
+import java.util.List;
+
+import com.pablofiumara.modelo.Libro;
+
+public interface LibroServicio {
+
+	public void agregarLibro(Libro unLibro);
+	public void editarLibro(Libro unLibro);
+	public Libro buscarLibro(int unId);
+	public void borrarLibro(int unId);
+	public List<Libro> listarLibros();
+
+}
---
+++ src/main/java/com/pablofiumara/service/LibroServicioImpl.java
@@ -0,0 +1,40 @@
+package com.pablofiumara.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pablofiumara.dao.LibroDAO;
+import com.pablofiumara.modelo.Libro;
+
+@Service
+@Transactional
+public class LibroServicioImpl implements LibroServicio {
+
+	@Autowired
+	private LibroDAO libroDAO;
+
+	public void agregarLibro(Libro unLibro) {
+		libroDAO.agregarLibro(unLibro);
+	}
+
+	public void editarLibro(Libro unLibro) {
+		libroDAO.editarLibro(unLibro);
+	}
+
+	public Libro buscarLibro(int unId) {
+		return libroDAO.buscarLibro(unId);
+	}
+
+	public void borrarLibro(int unId) {
+		libroDAO.borrarLibro(unId);
+	}
+
+	public List<Libro> listarLibros() {
+		return libroDAO.listarLibros();
+	}
+
+}
