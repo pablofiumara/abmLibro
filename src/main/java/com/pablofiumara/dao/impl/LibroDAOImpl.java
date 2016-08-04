86420ecd1728c1c64c6a1657895739c64a989574
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-04 11:55:07 -0300

    los metodos del dao no tienen que ser transactional pero si los del servicio

--- src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
+++ src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
@@ -7,26 +7,24 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.pablofiumara.dao.LibroDAO;
 import com.pablofiumara.modelo.Libro;
 
 
 @Repository
-@Transactional
 public class LibroDAOImpl implements LibroDAO {
 
 	@Autowired
 
 	private SessionFactory sessionFactory;
 
-	@Transactional
+
 	private Session getCurrentSession() {
 		return sessionFactory.getCurrentSession();
 	}
 
-	@Transactional
+
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
