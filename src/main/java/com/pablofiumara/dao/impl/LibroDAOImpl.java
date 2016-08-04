1f914292945acead1fdae747449fb20907900648
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-04 12:32:56 -0300

    Ahora funciona el getTeams() pero tira NullPointerException

--- src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
+++ src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
@@ -15,52 +15,23 @@ import com.pablofiumara.modelo.Libro;
 @Repository
 public class LibroDAOImpl implements LibroDAO {
 
-	@Autowired
 	private SessionFactory sessionFactory;
 
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-	private Session getCurrentSession() {
-		return sessionFactory.getCurrentSession();
-	}
+    @SuppressWarnings("unchecked")
+    public List<Libro> getTeams() {
+        Session session = sessionFactory.openSession();
+        try {
+            Query query = session.createQuery("from Libro l");
+            List<Libro> bookList = query.list();
+            return bookList;
+        } finally {
+            session.close();
+        }
+    }
 
-
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<Libro> getTeams() {
-		return getCurrentSession().createQuery("from Libro").list();
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<Libro> mostrarLosLibros() {
-		Session session = this.sessionFactory.getCurrentSession();
-
-		List<Libro> unaLista = session.createQuery("from Libro").list();
-
-		return unaLista;
-	}
-
-	public Integer getMaxId() {
-		Session session = this.sessionFactory.getCurrentSession();
-		String sql = "Select max(l.id) from Libro l ";
-		Query query = session.createQuery(sql);
-		Integer maxLibroId = (Integer) query.uniqueResult();
-		if (maxLibroId == null) {
-			return 0;
-		}
-		return maxLibroId;
-	}
-
-	public void agregarLibro(String unTitulo) {
-
-		Libro unLibro = new Libro();
-		unLibro.setId(getMaxId() + 1);
-		unLibro.setTitulo(unTitulo);
-
-		Session session = this.sessionFactory.getCurrentSession();
-		session.persist(unLibro);
-	}
 
 }
