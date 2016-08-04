13b4b3ae0d4b6527ec784860f689486184fad02d
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-04 18:43:46 -0300

    el setter de sessionFactory es necesario para evitar el error 'sessionFactory is not writable or has an invalid setter method'

--- src/main/java/com/pablofiumara/dao/LibroDAO.java
+++ src/main/java/com/pablofiumara/dao/LibroDAO.java
@@ -7,11 +7,5 @@ import com.pablofiumara.modelo.Libro;
 
 public interface LibroDAO {
 
-  public List<Libro> mostrarLosLibros() ;
-
-  public Integer getMaxId();
-
-  public void agregarLibro(String unTitulo);
-
   public List<Libro> getTeams();
 }
--- src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
+++ src/main/java/com/pablofiumara/dao/impl/LibroDAOImpl.java
@@ -2,36 +2,30 @@ package com.pablofiumara.dao.impl;
 
 import java.util.List;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pablofiumara.dao.LibroDAO;
 import com.pablofiumara.modelo.Libro;
 
-
 @Repository
 public class LibroDAOImpl implements LibroDAO {
 
+	@Autowired
 	private SessionFactory sessionFactory;
 
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+	public void setSessionFactory(SessionFactory sessionFactory) {
+	    this.sessionFactory = sessionFactory;
+	}
 
-    @SuppressWarnings("unchecked")
-    public List<Libro> getTeams() {
-        Session session = sessionFactory.openSession();
-        try {
-            Query query = session.createQuery("from Libro l");
-            List<Libro> bookList = query.list();
-            return bookList;
-        } finally {
-            session.close();
-        }
-    }
 
+	@SuppressWarnings("unchecked")
+	@Transactional
+	public List<Libro> getTeams() {
+		return sessionFactory.getCurrentSession().createQuery("from Libro").list();
+	}
 
 }
