f25575a9135d31f6ef9e50aa59f0e2c252620016
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-04 08:43:19 -0300

    paso 1/5: corregi el modelo (emplolije los getters y setters, saque los constructores, indique la columna que es id y cuyos valores se tienen que autogerar)

--- src/main/java/com/pablofiumara/modelo/Libro.java
+++ src/main/java/com/pablofiumara/modelo/Libro.java
@@ -1,43 +1,34 @@
 package com.pablofiumara.modelo;
 
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "Libros")
 public class Libro {
 
-   private Integer Id;
-   private String titulo;
+	@Id
+	@GeneratedValue
+	private Integer Id;
+
+	private String titulo;
+
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		this.Id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
 
-   public Libro() {
-   }
 
-   public Libro(Integer unId, String unTitulo) {
-       this.Id = unId;
-       this.titulo = unTitulo;
-   }
-
-   @Id
-   @Column(name = "numeroDeId", nullable = false)
-   public Integer getId() {
-       return Id;
-   }
-
-   public void setId(Integer id) {
-       this.Id = id;
-   }
-
-   @Column(name = "tituloDelLibro", length = 20)
-   public String getTitulo() {
-       return titulo;
-   }
-
-   public void setTitulo(String unTitulo) {
-       this.titulo = unTitulo;
-   }
 }
