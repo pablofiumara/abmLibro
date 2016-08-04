a597fa203da87c675e8e68bb62f6d85759d6f2d5
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-04 18:37:55 -0300

    borre imports que no se usan

--- src/main/java/com/pablofiumara/controladores/ControladorPrincipal.java
+++ src/main/java/com/pablofiumara/controladores/ControladorPrincipal.java
@@ -6,14 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.pablofiumara.dao.LibroDAO;
+
 import com.pablofiumara.modelo.Libro;
 import com.pablofiumara.service.LibroServicio; //importo la interfaz
 
