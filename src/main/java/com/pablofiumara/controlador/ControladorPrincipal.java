3b21bc490557d8ce06086d6fd36644a6dd6953f8
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-05 13:25:13 -0300

    Controlador: agregué un método que se usará cuando el usuario ingrese a /agregarlibro, vista que muestra un formulario para que el usuario ingrese un nuevo libro a la base de datos

--- src/main/java/com/pablofiumara/controlador/ControladorPrincipal.java
+++ src/main/java/com/pablofiumara/controlador/ControladorPrincipal.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pablofiumara.modelo.Libro;
@@ -31,4 +32,12 @@ public class ControladorPrincipal {
 	public String home(Model model) {
 		return "index";
 	}
+
+	@RequestMapping(value="/agregarlibro", method=RequestMethod.GET)
+	public ModelAndView agregarLibroPagina() {
+		ModelAndView modeloYVista = new ModelAndView("agregarLibroFormulario");
+		modeloYVista.addObject("libroNuevo", new Libro());
+		return modeloYVista;
+	}
+
 }
