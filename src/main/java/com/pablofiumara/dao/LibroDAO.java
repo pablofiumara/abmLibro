5ca51028a8c3404ed0770348ac8bbc18c801919d
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-05 17:45:10 -0300

    DAO: cambié el nombre de un parámetro

--- src/main/java/com/pablofiumara/dao/LibroDAO.java
+++ src/main/java/com/pablofiumara/dao/LibroDAO.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import com.pablofiumara.modelo.Libro;
 
-
 public interface LibroDAO {
 
-  public List<Libro> buscarLosLibros();
-	public void addTeam(Libro team);
+	public List<Libro> buscarLosLibros();
+
+	//no es necesario que el nombre de este parámetro sea igual al nombre del parámetro de su implementacion
+	public void addTeam(Libro libro);
 
 }
