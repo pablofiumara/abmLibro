87a60df3a43f8595ad9bf75eaa5f80ce36893374
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-10 17:36:37 -0300

    Validacion: no son necesarios los getters y setters (no los uso)

--- src/main/java/com/pablofiumara/formulario/AgregarLibroFormulario.java
+++ src/main/java/com/pablofiumara/formulario/AgregarLibroFormulario.java
@@ -4,15 +4,6 @@ import javax.validation.constraints.NotNull;
 
 public class AgregarLibroFormulario {
 
-
   @NotNull
   private String titulo;
-
-  public String getTitulo() {
-    return titulo;
-  }
-
-  public void setTitulo(String titulo) {
-    this.titulo = titulo;
-  }
 }
