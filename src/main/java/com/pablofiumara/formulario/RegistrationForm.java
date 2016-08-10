6719665a7744cd6a7b0dbcbdb4d2ba9ef533e0c4
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-10 17:19:40 -0300

    Elimino código que no necesito

--- src/main/java/com/pablofiumara/formulario/RegistrationForm.java
+++ src/main/java/com/pablofiumara/formulario/RegistrationForm.java
@@ -1,14 +1,7 @@
 package com.pablofiumara.formulario;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-/**
- * Basic Registration Form with JSR-303 Validation
- *
- * @author Bubba Hines <bubba@hines57.com>
- * @since 0.02
- */
 public class RegistrationForm {
 
 
