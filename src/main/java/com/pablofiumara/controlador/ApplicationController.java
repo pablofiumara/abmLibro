e1067bec37e2d9a20bab42982b4ffc20789f06e3
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-13 16:13:15 -0300

    Servicio web: mi código funciona pero webservicex.net no

---
+++ src/main/java/com/pablofiumara/controlador/ApplicationController.java
@@ -0,0 +1,53 @@
+package com.pablofiumara.controlador;
+
+
+import com.pablofiumara.wsdl.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pablofiumara.servicio.*;
+
+@Controller
+public class ApplicationController {
+    
+    @Autowired
+    CurrencyService currencyService;
+
+    @RequestMapping(value = "/test1", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+        model.addAttribute("msgArgument",
+                "Maven Java Web Application Project: Success!");
+        return "test";
+
+    }
+
+    @RequestMapping(value = "/test2/{arg}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String arg, ModelMap model) {
+        model.addAttribute("msgArgument",
+                "Maven Java Web Application Project, input variable: " + arg);
+
+        return "test";
+    }
+    
+    @RequestMapping(value = "/convert/{fromCur}/{toCur}/{amount}", method = RequestMethod.GET)
+    public String convert(@PathVariable String fromCur,@PathVariable String toCur, @PathVariable Double amount,ModelMap model) {
+        
+        
+        
+        Currency fromCurrency = Currency.fromValue(fromCur.toUpperCase());
+        Currency toCurrency = Currency.fromValue(toCur.toUpperCase());
+        Double conversionRate = currencyService.getConversionRate(fromCurrency, toCurrency);
+        
+        Double result = amount * conversionRate;
+ 
+        model.addAttribute("msgArgument","Maven Java Web Application Project, input variable: " + result);
+
+        return "test";
+    }
+
+}
