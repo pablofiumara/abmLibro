ce39f003c88b51d1077e11c83517d65820bf0b20
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-17 18:05:56 -0300

    PDF: configuro controlador con @Controller por lo cual no tengo que declarar el bean en *-servlet.xml

--- src/main/java/com/pablofiumara/controlador/RevenueReportController.java
+++ src/main/java/com/pablofiumara/controlador/RevenueReportController.java
@@ -1,42 +1,77 @@
 package com.pablofiumara.controlador;
 
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.CopyUtils;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-public class RevenueReportController extends AbstractController{
+@Controller
+public class RevenueReportController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/pdf")
+    public void generatePdf(HttpServletRequest req,HttpServletResponse res)
+    {
+        res.setContentType("text/html;charset=UTF-8");
+        ServletOutputStream outStream=null;
+        try 
+        {
+            String calledFrom = req.getHeader("referer");
+            calledFrom=req.getRequestURL().substring(0,req.getRequestURL().lastIndexOf("/"))+"/ReportGenerator.egp";
+            calledFrom += "?isPdf=yes&"+req.getQueryString();
+            System.out.println(calledFrom+"?isPdf=yes&"+req.getQueryString());
 
-		String output =
-			ServletRequestUtils.getStringParameter(request, "output");
 
-		//dummy data
-		Map<String,String> revenueData = new HashMap<String,String>();
-		revenueData.put("1/20/2010", "$100,000");
-		revenueData.put("1/21/2010", "$200,000");
-		revenueData.put("1/22/2010", "$300,000");
-		revenueData.put("1/23/2010", "$400,000");
-		revenueData.put("1/24/2010", "$500,000");
 
-		if(output ==null || "".equals(output)){
-		    //return normal view
-		    return new ModelAndView("RevenueSummary","revenueData",revenueData);
+            InputStream input = new URL(calledFrom).openStream();
+            StringWriter writer = new StringWriter();
+            CopyUtils.copy(input, writer);
 
-		}else if("PDF".equals(output.toUpperCase())){
-		    //return excel view
-		    return new ModelAndView("RevenueSummary","revenueData",revenueData);
+            //System.out.println(writer.toString());
 
-		}else{
-		    //return normal view
-		    return new ModelAndView("RevenueSummary","revenueData",revenueData);
+            res.setContentType("application/pdf");
+            res.setHeader("Content-Disposition", "attachment;filename=report.pdf");
+            outStream = res.getOutputStream();
 
-		}
-	}
+            ITextRenderer renderer = new ITextRenderer();
+
+
+            renderer.setDocument(calledFrom);
+            renderer.layout();
+            renderer.createPDF(outStream);
+
+        } 
+        catch (Exception e) 
+        {
+        	System.out.println("Exception");
+        }
+        finally
+        {
+            try
+            {
+                outStream.flush();
+                outStream.close();
+            }
+            catch(Exception ex)
+            {
+                System.out.println("Exception");
+            }
+
+        }
+
+
+    }
+
 }
