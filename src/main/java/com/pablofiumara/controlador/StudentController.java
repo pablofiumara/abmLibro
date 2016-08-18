221917d4842b7d758a54558b5aa4a61725731f11
Pablo Alejandro Fiumara <pablo.fiumara@gmail.com>
2016-08-17 22:38:38 -0300

    Eliminé método en el controlador que no uso (y un import por el mismo motivo

--- src/main/java/com/pablofiumara/controlador/StudentController.java
+++ src/main/java/com/pablofiumara/controlador/StudentController.java
@@ -2,7 +2,6 @@ package com.pablofiumara.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,14 +12,7 @@ import com.pablofiumara.bean.Student;
 public class StudentController {
 
 	@Autowired
-	private Student student;
-
-	@RequestMapping(value = "/html", produces = "application/xml")
-	public String html(Model model) {
-		student.setName("Corey Brown");
-		model.addAttribute("stud", student);
-		return "studentPage";
-	}
+	private Student student;	
 
 	@RequestMapping("/rich")
 	public ModelAndView beanToPdf() {
